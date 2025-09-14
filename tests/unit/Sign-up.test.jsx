f3dbf1d8cf4f652624e3cdf6bd5953075c713960
Goran Jovic <goran1010jovic@gmail.com>
2025-09-14 12:54:24 +0200

    test: add tests for heading, labels and inputs in SignUp

--- tests/unit/Sign-up.test.jsx
+++ tests/unit/Sign-up.test.jsx
@@ -5,7 +5,19 @@ import SignUp from "../../src/components/SignUp";
 describe("Sign-up component", () => {
   test("Render component", () => {
     render(<SignUp />);
-    const h1 = screen.getByText("Create your account");
-    expect(h1).toBeInTheDocument();
+
+    const heading = screen.getByRole("heading", {
+      value: "Create your account",
+    });
+    const username = screen.getByLabelText("Username");
+    const email = screen.getByLabelText("Email");
+    const password = screen.getByLabelText("Password");
+    const confirmPassword = screen.getByLabelText("Confirm Password");
+
+    expect(heading).toBeInTheDocument();
+    expect(username).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(confirmPassword).toBeInTheDocument();
   });
 });
