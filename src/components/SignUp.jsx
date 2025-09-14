1853ea5ce222ca57d1e471c405f5f18c4bc5590d
Goran Jovic <goran1010jovic@gmail.com>
2025-09-14 12:33:22 +0200

    feat: add basic labels and inputs to SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,3 +1,31 @@
 export default function SignUp() {
-  return <main>Sign Up</main>;
+  return (
+    <main>
+      <div>
+        <h1>Create your account</h1>
+      </div>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="email">Username</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Username</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <div>
+          <label htmlFor="confirm-password">Username</label>
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+          />
+        </div>
+      </form>
+    </main>
+  );
 }
