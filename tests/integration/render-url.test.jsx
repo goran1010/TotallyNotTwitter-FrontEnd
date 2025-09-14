ee594d32c2f291af440feccf8e9c5b43cde09c71
Goran Jovic <goran1010jovic@gmail.com>
2025-09-14 13:48:38 +0200

    test: rename a test and add render test for SignUp page

--- tests/integration/render-url.test.jsx
+++ tests/integration/render-url.test.jsx
@@ -4,13 +4,26 @@ import { createMemoryRouter } from "react-router-dom";
 import routes from "../../src/routes";
 import { RouterProvider } from "react-router-dom";
 
-describe("Navigation", () => {
+describe("Render components through url", () => {
   test("Render Error Page", () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/non-existent-url"],
     });
     render(<RouterProvider router={router} />);
+
     const text = screen.getByText("This is a custom 404 error page.");
     expect(text).toBeInTheDocument();
+  });
+
+  test("Render Signup Page", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+    });
+    render(<RouterProvider router={router} />);
+
+    const heading = screen.getByRole("heading", {
+      name: "Create your account",
+    });
+    expect(heading).toBeInTheDocument();
   });
 });
