96744b385715c2c5bcc3354b01a4d023c762380c
Goran Jovic <goran1010jovic@gmail.com>
2025-09-29 18:49:14 +0200

    test: add LogIn component render unit test

---
+++ tests/unit/LogIn.test.jsx
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LogIn from "../../src/components/LogIn";
+
+describe("Log In component", () => {
+  test("Render component", () => {
+    render(<LogIn />);
+
+    const heading = screen.getByRole("heading", {
+      value: "Log in",
+    });
+    const username = screen.getByLabelText("Username");
+    const password = screen.getByLabelText("Password");
+    const button = screen.getByRole("button", { value: "Log in" });
+
+    expect(heading).toBeInTheDocument();
+    expect(username).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
