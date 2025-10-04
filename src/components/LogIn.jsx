87cdcb96a35d1252720d50b30a6f351334773b24
Goran Jovic <goran1010jovic@gmail.com>
2025-10-04 12:47:35 +0200

    feat: add Link to and back login and signup

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function LogIn() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50 ">
-      <div className="w-full max-w-md p-6">
+      <div className="w-full max-w-md p-6 flex flex-col gap-3">
         <h1 className="text-5xl mb-8 text-center font-bold text-gray-900">
           Please log in
         </h1>
@@ -37,12 +39,24 @@ export default function LogIn() {
           <div>
             <button
               type="button"
-              className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 hover:cursor-pointer active:scale-98"
             >
               Log in
             </button>
           </div>
         </form>
+        <div>
+          <p className="text-center">
+            Don't have an account ? Go to the{" "}
+            <Link
+              className="text-blue-600 hover:underline font-bold"
+              to={"/signup"}
+            >
+              Sign Up
+            </Link>{" "}
+            page.
+          </p>
+        </div>
       </div>
     </main>
   );
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,34 +1,93 @@
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
   return (
-    <main>
-      <div>
-        <h1>Create your account</h1>
+    <main className="min-h-screen flex items-center justify-center bg-gray-50 ">
+      <div className="w-full max-w-md p-6 flex flex-col gap-3">
+        <div>
+          <h1 className="text-5xl mb-8 text-center font-bold text-gray-900">
+            Create your account
+          </h1>
+        </div>
+        <form className="flex flex-col gap-3">
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="block w-full border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="block w-full border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="confirm-password"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirm-password"
+              id="confirm-password"
+              className="block w-full border border-gray-300 rounded-md p-2"
+            />
+          </div>
+          <div>
+            <button
+              type="button"
+              className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 hover:cursor-pointer active:scale-98"
+            >
+              Create
+            </button>
+          </div>
+        </form>
+        <div>
+          <p className="text-center">
+            Already have an account ? Go back to the{" "}
+            <Link
+              className="text-blue-600 hover:underline font-bold"
+              to={"/login"}
+            >
+              Log In
+            </Link>{" "}
+            page.
+          </p>
+        </div>
       </div>
-      <form>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
-        </div>
-        <div>
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <input
-            type="password"
-            name="confirm-password"
-            id="confirm-password"
-          />
-        </div>
-        <div>
-          <button type="button">Create</button>
-        </div>
-      </form>
     </main>
   );
 }
