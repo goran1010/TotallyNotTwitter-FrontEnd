33830fc4bae308c2a0b9d5f5b38e384c0c6f6e8f
Goran Jovic <goran1010jovic@gmail.com>
2025-10-07 10:43:24 +0200

    test: add test for singup to login redirect on success

--- tests/integration/render-url.test.jsx
+++ tests/integration/render-url.test.jsx
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { createMemoryRouter } from "react-router-dom";
 import routes from "../../src/routes";
 import { RouterProvider } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, vi } from "vitest";
 
 describe("Render components through url", () => {
   test("Error Page for non-existent url", () => {
@@ -34,6 +36,36 @@ describe("Render components through url", () => {
     render(<RouterProvider router={router} />);
 
     const heading = screen.getByRole("heading", {
+      name: "Please log in",
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe("redirects after fetching", () => {
+  beforeEach(() => {
+    global.fetch = vi.fn();
+  });
+
+  test("SignUp redirect to LogIn after success", async () => {
+    global.fetch.mockResolvedValueOnce({
+      ok: true,
+      status: 201,
+      json: async () => ({ message: "User created" }),
+    });
+
+    const user = userEvent.setup();
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup"],
+    });
+    render(<RouterProvider router={router} />);
+
+    const button = screen.getByRole("button", { name: /create/i });
+    expect(button).toBeInTheDocument();
+
+    await user.click(button);
+
+    const heading = await screen.findByRole("heading", {
       name: "Please log in",
     });
     expect(heading).toBeInTheDocument();
