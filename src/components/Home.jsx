18d1c2c249e83f336badfc29f80638b6555b4b00
Goran Jovic <goran1010jovic@gmail.com>
2025-10-11 14:46:31 +0200

    feat: add basic component structure to routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  );
+  return <div>Home</div>;
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,10 @@ import ErrorPage from "./components/ErrorPage";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Home from "./components/Home";
+import Main from "./components/Main";
+import Profile from "./components/Profile";
+import Followers from "./components/Followers";
+import Following from "./components/Following";
 
 const routes = [
   {
@@ -10,9 +14,22 @@ const routes = [
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/signup", element: <SignUp /> },
-      { path: "/login", element: <LogIn /> },
-      { default: true, path: "/", element: <Home /> },
+      { path: "signup", element: <SignUp /> },
+      { path: "login", element: <LogIn /> },
+      {
+        path: "/",
+        element: <Main />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          { path: "home", element: <Home /> },
+          { path: "profile", element: <Profile /> },
+          { path: "followers", element: <Followers /> },
+          { path: "following", element: <Following /> },
+        ],
+      },
     ],
   },
   { path: "*", element: <ErrorPage /> },
