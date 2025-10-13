5833e142e1d6baf1c3ca7817551cc10371e3667f
Goran Jovic <goran1010jovic@gmail.com>
2025-10-13 11:18:37 +0200

    feat: add basic styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to={"/home"}>Home</Link>
-      <Link to={"/followers"}>Followers</Link>
-      <Link to={"/following"}>Following</Link>
-      <Link to={"/profile"}>Profile</Link>
+    <nav
+      className="
+        flex w-full justify-around p-4 bg-gray-200
+        md:justify-center md:gap-8
+        lg:flex-col lg:fixed lg:top-10 lg:left-3 lg:items-center lg:space-y-6 lg:w-min
+      "
+    >
+      <Link to="/home">Home</Link>
+      <Link to="/followers">Followers</Link>
+      <Link to="/following">Following</Link>
+      <Link to="/profile">Profile</Link>
     </nav>
   );
 }
