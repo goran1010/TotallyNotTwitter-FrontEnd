e276b17e955c7752f1c38646941627ae0a90db17
Goran Jovic <goran1010jovic@gmail.com>
2025-10-24 09:08:50 +0200

    fix: change width and styling of main and root

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Main() {
   return (
-    <main>
+    <main className="w-full lg:w-3xl bg-gray-100">
       <Navbar />
       <Outlet />
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       className="
         flex w-full justify-around p-4 bg-gray-200
         md:justify-center md:gap-8
-        lg:flex-col lg:fixed lg:top-10 lg:left-3 lg:items-center lg:space-y-6 lg:w-min
+        lg:flex-col lg:fixed lg:top-1/2 lg:left-0 lg:translate-y-[-50%] lg:items-center lg:space-y-6 lg:w-min
       "
     >
       <Link to="/home">Home</Link>
